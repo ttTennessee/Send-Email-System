e4cf53ad2dca4813a2d344559dd031e7de7351f6
Jin <929760196@qq.com>
2019-05-13 13:30:29 +0800

    1.创建项目结构 2.整合Druid + tk-mybatis + pagehelper 3.本地Mysql连接成功并且生成Mapper,Dao,Entity

---
+++ send-email-web/src/main/java/xyz/jfshare/demos/send/email/web/SendWebApplication.java
@@ -0,0 +1,11 @@
+package xyz.jfshare.demos.send.email.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendWebApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SendWebApplication.class,args);
+    }
+}
